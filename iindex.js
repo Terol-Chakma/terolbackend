465c757e395f196c77317c161cf1d0da1c986938
Terol Chakma <chakmaterol1@gmail.com>
2025-06-27 20:31:26 +0530

    updated

--- iindex.js
+++ iindex.js
@@ -5,6 +5,8 @@ const express = require('express') //a variable(import express) using require fn
 const app = express()   // import app , a fn of express --> 
 const port = 3000   // import port : sometimes may busy this port on that time have to use another port using .env --> port = 4000
 
+const githubData = 
+
 app.get('/', (req, res) => {    // get is a request, '/' is home route, use with callback
   res.send('Hello World!')  // send is for print/output 'res' is the place-> where the response called
 })
@@ -25,6 +27,10 @@ app.get('/youtube', (req, res) => {
 // app.listen(port, () => {    //listen --> server is always listening(have to)
 //   console.log(`Example app listening on port ${port}`)
 // })
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
 
 app.listen(process.env.PORT, () => {  
   console.log(`Example app listening on port ${port}`)
